052f4923f7c1253df8dddacd9574b40fd39c4edc
ChineduSE <chinedunweke618@gmail.com>
2025-01-03 16:42:53 +0100

    Local updates

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -40,3 +40,42 @@ switch (userType) {
         userCategory = "Unknown";
 }
 console.log("User Category:", userCategory);
+
+// Practice Task
+
+let userRoles = "New"
+let authorizedAcess;
+
+switch (userRoles) {
+    case "Employee":
+        authorizedAcess = "You have authorized access to dietary services"
+        break;
+    case "Enrolled Member":
+        authorizedAcess = "You have authorized access to dietary service and one-on-ine interaction with dietician"
+        break;
+    case "Subscriber":
+        authorizedAcess = "You are authorized to partial access to dietary services"
+        break;
+    default:
+        authorizedAcess = "Please subscribe to have access to dietary services"
+}
+
+console.log(authorizedAcess)
+
+// Practice Task Second Approach
+
+let aSubscriber = true;
+let userStatus = "Enrolled Member";
+let haveAccess;
+
+if (aSubscriber) {
+    if(userStatus === "Employee")
+        haveAccess = "You have authorized access to dietary services";
+    else if(userStatus === "Enrolled Member")
+         haveAccess = "You have authorized access to dietary service and one-on-ine interaction with dietician";
+    else
+        haveAccess = "You are authorized to partial access to dietary services";
+}
+else haveAccess = "Please subscribe!";
+
+console.log(haveAccess);
