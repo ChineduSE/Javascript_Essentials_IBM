f58091f9ea6d066b8e70e6426ca91b07a75c03e8
ChineduSE <chinedunweke618@gmail.com>
2025-01-10 23:32:10 +0100

    calculate area

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -11,3 +11,16 @@ function calculateArea() {
 //    console.log(area);
    document.getElementById('result').innerText = `The area of the rectangle is only: ${area}`;
 }
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateGroceryTotal(){
+grocery1 = parseFloat(document.getElementById("Grocery-1").value);
+grocery2 = parseFloat(document.getElementById("Grocery-2").value);
+grocery3 = parseFloat(document.getElementById("Grocery-3").value);
+
+let total = grocery1+grocery2+grocery3;
+document.getElementById("groceryResult").innerText = `The total amount of grocery is: $${total}`;
+}
